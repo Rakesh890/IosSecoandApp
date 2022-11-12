d08f7aab2a524fb1b2c4b49d6f2fe29c1f60a56d
FirstiOSApp <rakeshprajapat78122@gmail.com>
2022-11-12 21:17:25 +0530

    Initial Commit

---
+++ IosSecoandApp/IosSecoandAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosSecoandAppApp.swift
+//  IosSecoandApp
+//
+//  Created by Rakesh on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct IosSecoandAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
